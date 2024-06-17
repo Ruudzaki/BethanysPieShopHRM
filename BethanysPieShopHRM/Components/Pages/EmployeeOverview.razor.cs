de6939c934a86dbec90c0008508f54d1b92c0a74
Kostiantyn B. <kostybelous@gmail.com>
2024-06-17 11:31:15 +0300

    add employee detail page

--- BethanysPieShopHRM/Components/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM/Components/Pages/EmployeeOverview.razor.cs
@@ -5,10 +5,10 @@ namespace BethanysPieShopHRM.Components.Pages;
 
 public partial class EmployeeOverview
 {
-    public List<Employee> Employees { get; set; } = default!;
-
+    public List<Employee> Employees { get; set; } = default;
     protected override async Task OnInitializedAsync()
     {
+        await Task.Delay(2000).ConfigureAwait(false);
         Employees = MockDataService.Employees;
     }
 }
